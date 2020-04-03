0e03f57fb769fe46f274b8be7d5ded5b0d83cd85
Roberto Nogueira <enogrob@gmail.com>
2020-04-02 23:35:59 -0300

    update-backups

--- backups/shell/osx-home/.obras_osx.sh
+++ backups/shell/osx-home/.obras_osx.sh
@@ -4,10 +4,11 @@
 # variables
 export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
 export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
+export SITE=demo
 export MYSQL_DATABASE_DEV=demo_dev
 export MYSQL_DATABASE_TST=demo_tst
 export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
-# export RAILS_ENV=demo
+export RAILS_ENV=development
 export RUBYOPT=-W0
 
 # aliases development
@@ -24,47 +25,138 @@ alias dki='docker image'
 alias dkis='docker images'
 
 # functions
+mysql_ls(){
+  brew services list
+}
+
+__pr(){
+    if [ $# -eq 0 ]; then
+        echo -e ""
+    elif [ $# -eq 2 ]; then
+        case $1 in
+            dang|red)
+                echo -e "\033[31m$2 \033[0m"
+                ;;
+            succ|green)
+                echo -e "\033[32m$2 \033[0m"
+                ;;
+            warn|yellow)
+                echo -e "\033[33m$2 \033[0m"
+                ;;
+            info|blue)
+                echo -e "\033[34m$2 \033[0m"
+                ;;
+            bold|white)
+                echo -e "\033[1;39m$2 \033[0m"
+                ;;
+        esac
+    elif [ $# -eq 3 ]; then
+        case $1 in
+            dang|red)
+                echo -e "$2 \033[31m$3 \033[0m"
+                ;;
+            succ|green)
+                echo -e "$2 \033[32m$3 \033[0m"
+                ;;
+            warn|yellow)
+                echo -e "$2 \033[33m$3 \033[0m"
+                ;;
+            info|blue)
+                echo -e "$2 \033[36m$3 \033[0m"
+                ;;
+            bold|white)
+                echo -e "$2 \033[1;39m$3 \033[0m"
+                ;;
+        esac
+    else
+        __pr red "=> Error: Bad number of arguments."
+        __pr
+        return 1
+    fi
+}
+
 function restart_mysql {
   FILE=$HOME/Library/LaunchAgents/homebrew.mxcl.mysql@5.7.plistnf
   if test -f "$FILE"; then
     unload ~/Library/LaunchAgents/homebrew.mxcl.mysql@5.7.plist
     rm ~/Library/LaunchAgents/homebrew.mxcl.mysql@5.7.plist
-    brew services start mysql@5.7
-  else  
-    brew services restart mysql@5.7
-  fi
+  fi 
+  brew services stop mysql@5.7
+  brew services start mysql@5.7
   brew services list
 }
 
+function initdb(){
+  rake db:drop
+  rake db:create
+  rake db:migrate
+  rake db:seed
+}
+
 function importdb(){
-  rails db:environment:set RAILS_ENV=development
-  rails db:drop
-  rails db:create
+  rake db:drop
+  rake db:create
   mysql -u root -p $MYSQL_DATABASE_DEV < $1
-  rails db:migrate
+  rake db:migrate
 }
 
 function dkimportdb(){
-  docker-compose exec $1 bundle exec rails db:environment:set RAILS_ENV=$RAILS_ENV
-  docker-compose exec $1 bundle exec rails db:drop
-  docker-compose exec $1 bundle exec rails db:drop
-  docker-compose exec $1 bundle exec rails db:create
+  docker-compose exec $1 bundle exec rake db:drop
+  docker-compose exec $1 bundle exec rake db:drop
+  docker-compose exec $1 bundle exec rake db:create
   docker exec -i db mysql -uroot -proot $2 < $3
-  docker-compose exec $1 bundle exec rails db:migrate
+  docker-compose exec $1 bundle exec rake db:migrate
 }
 
 function db(){
-  echo $MYSQL_DATABASE_DEV
-  echo $MYSQL_DATABASE_TST
+  __pr succ "Database Dev:" $MYSQL_DATABASE_DEV 
+  __pr succ "Database Tst:" $MYSQL_DATABASE_TST 
+  __pr
+}
+
+function site(){
+  __pr info "Proj:" $(basename $PWD)
+  __pr bold "Site:" $SITE
+  __pr dang "Rvm :" $(rvm current)
+  __pr warn "Env :" $RAILS_ENV
+  db
 }
 
 function setdb(){
   set -o allexport
   . ./.env/development/$1
   set +o allexport
-  db
 }
 
 function dash(){
   open dash://$1:$2
+}
+
+function settitle(){
+  SITE=$1
+  export PROMPT_COMMAND='echo -ne "\033]0;${SITE##*/}\007"'
+}
+
+function setsite {
+  unset SITE
+  export SITE=$1
+  case $1 in
+    santoandre|olimpia|demo)
+      cd "$HOME/Projects/obras"
+      setdb $1
+      settitle $1
+      site
+      ;;
+    rioclaro|suzano)
+      cd "$HOME/Logbook/obras"
+      setdb $1
+      settitle $1
+      site
+      ;;
+    *) 
+      __pr dang "=> Error: Bad site "$1
+      __pr
+      return 1
+      ;;
+  esac
 }
